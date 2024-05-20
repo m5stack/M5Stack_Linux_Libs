4bb0d155a7ec8df31427f63b2d740a112460557b
dianjixz <dianjixz@m5stack.com>
2024-05-20 09:24:18 +0800

    [add] show_logo

---
+++ examples/show_logo/config_defaults.mk
@@ -0,0 +1,11 @@
+# unix
+CONFIG_TOOLCHAIN_PATH="/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin"
+# win
+# CONFIG_TOOLCHAIN_PATH="..\\gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf\\bin"
+
+CONFIG_TOOLCHAIN_PREFIX="arm-linux-gnueabihf-"
+
+# CONFIG_MINICV2_COMPONENT_ENABLED=y
+CONFIG_DEVICE_DRIVER_ENABLED=y
+CONFIG_DEVICE_FRAMEBUFFER_ENABLED=y
+CONFIG_STB_ENABLED=y
