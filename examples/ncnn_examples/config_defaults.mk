50de5e5d00f502ca3e55fb0e14f523a1e2d17736
dianjixz <dianjixz@m5stack.com>
2024-05-31 19:04:00 +0800

    [add] ncnn test

---
+++ examples/ncnn_examples/config_defaults.mk
@@ -0,0 +1,10 @@
+# unix
+# CONFIG_TOOLCHAIN_PATH="/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin"
+# win
+# CONFIG_TOOLCHAIN_PATH="..\\gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf\\bin"
+
+CONFIG_TOOLCHAIN_PREFIX="arm-linux-gnueabihf-"
+
+# CONFIG_MINICV2_COMPONENT_ENABLED=y
+CONFIG_NCNN_ENABLED=y
+CONFIG_NCNN_ARM_LINUX_GNUEABIHF=y
