ec90035acc04b72dd0c5d9313dd37e2beac90e2a
dianjixz <dianjixz@m5stack.com>
2024-06-05 18:50:20 +0800

    [add] firebird_demo

---
+++ examples/firebird_demo/README.md
@@ -0,0 +1,162 @@
+# firebird
+Firebird is a full-featured, powerful, efficient, lightweight, and maintenance-free database. It makes it easy for you to upgrade from a single user, single database to enterprise-level applications.
+
+A Firebird database server can manage multiple independent databases, each of which can support multiple client connections simultaneously. In short, it is an open-source, powerful, and freely available database (even for commercial use).
+
+Features:
+1. Firebird is a true relational database that supports stored procedures, views, triggers, transactions, and all the features of large relational databases.  
+2. Firebird supports the majority of SQL92 commands and most SQL99 commands, with the newer version Firebird 2.0 providing even more complete support for SQL99.  
+3. Firebird's source code is based on the mature commercial database Interbase, ensuring good stability and compatibility with Interbase.  
+4. No need to worry about licensing fees (it's free), and you don't have to fear being sued by the database developer for using pirated software in the future.  
+5. Easy to deploy, with installation files just a few megabytes in size, highly customizable, and simple client distribution requiring only a DLL file.  
+6. Firebird has an embedded server version that runs directly without installation, making it a preferred choice for single-machine development.  
+7. Firebird has very high operational efficiency.  
+8. Highly portable, capable of running on Linux, Unix, MacOS, Windows, Solaris systems with the same database format, requiring no modifications.  
+9. Excellent development environment support, where applications can be developed based on Firebird directly using native development interfaces without the need for ODBC connections, such as in Delphi and C++Builder.  
+
+
+## Installation
+The official website of Firebird provides precompiled binary files of the Firebird database, which are available for each platform. You just need to go to [firebird](https://www.firebirdsql.org/) to get the installation package of the corresponding platform.
+
+Installation on ARM:
+```bash
+wget https://github.com/FirebirdSQL/firebird/releases/download/v5.0.0/Firebird-5.0.0.1306-0-linux-arm32.tar.gz
+tar zxvf Firebird-5.0.0.1306-0-linux-arm32.tar.gz
+cd Firebird-5.0.0.1306-0-linux-arm32
+sudo ./install
+```
+
+## Local
+You can use /opt/firebird/bin/isql for management locally.  
+Create a database:
+```bash
+cd /opt/firebird/bin
+./isql
+SQL> CREATE DATABASE '/opt/firebird/test_database.fdb' USER 'test' PASSWORD 'test';
+SQL> quit;
+```
+
+Connect to the database for CRUD operations:
+```bash
+cd /opt/firebird/bin
+./isql /opt/firebird/test_database.fdb -u test -p test
+# View all tables
+SQL> SHOW TABLES;
+# If the test_table does not exist, create it
+SQL> SET TERM !! ; EXECUTE BLOCK AS BEGIN IF (NOT EXISTS (SELECT 1 FROM rdb$relations WHERE rdb$relation_name = 'TEST_TABLE')) THEN BEGIN EXECUTE STATEMENT 'CREATE TABLE test_table (id INTEGER NOT NULL PRIMARY KEY, name VARCHAR(100))'; END END!! SET TERM ; !!
+SQL> COMMIT;
+# Insert two rows of data
+SQL> INSERT INTO test_table (id, name) VALUES (1, 'John Doe');
+SQL> INSERT INTO test_table (id, name) VALUES (2, 'Jane Doe');
+# Commit the operation
+SQL> COMMIT;
+# View the data in the database
+SQL> SELECT * FROM test_table;
+# Modify the values in the database
+SQL> UPDATE test_table SET name = 'John Smith' WHERE id = 1;
+SQL> COMMIT;
+SQL> SELECT * FROM test_table;
+# Delete the values in the database
+SQL> DELETE FROM test_table WHERE id = 2;
+SQL> COMMIT;
+SQL> SELECT * FROM test_table;
+# Delete the table
+# SQL> COMMIT;
+# SQL> DROP TABLE test_table;
+# SQL> COMMIT;
+# exit
+SQL> QUIT;
+
+```
+
+
+## Remote 
+An account needs to be created for remote use：
+```bash
+cd /opt/firebird/bin
+./gsec -user sysdba -password masterkey -add newuser -pw newpassword
+./gsec -user sysdba -password masterkey -modify existinguser -pw newpassword
+./gsec -user sysdba -password masterkey -delete username
+./gsec -user sysdba -password masterkey -display
+```
+- sysdba is the username for the administrator.
+- masterkey is the password for the administrator.
+- newuser is the username to be added.
+- newpassword is the password for the new user.
+- existinguser is the user to be modified.
+- username is the user to be deleted.
+
+Install dependencies：
+```bash
+sudo apt install libtommath1
+wget https://github.com/FirebirdSQL/firebird/releases/download/v5.0.0/Firebird-5.0.0.1306-0-linux-x64.tar.gz
+tar zxvf Firebird-5.0.0.1306-0-linux-x64.tar.gz
+cd Firebird-5.0.0.1306-0-linux-x64
+tar zxvf buildroot.tar.gz
+export LD_LIBRARY_PATH=`pwd`/buildroot/opt/firebird/lib:$LD_LIBRARY_PATH
+sudo apt install python3 python3-pip
+pip3 install fdb
+```
+### python
+```python
+import fdb
+
+con = fdb.connect(
+    host='127.0.0.1',
+    database='/opt/firebird/test_database.fdb', 
+    user='test',
+    password='test'
+)
+
+cur = con.cursor()
+
+cur.execute('''
+SET TERM !! ;
+EXECUTE BLOCK AS
+BEGIN
+  IF (NOT EXISTS (
+    SELECT 1 FROM rdb$relations 
+    WHERE rdb$relation_name = 'TEST_TABLE'
+  )) THEN
+  BEGIN
+    EXECUTE STATEMENT '
+      CREATE TABLE test_table (
+        id INTEGER NOT NULL PRIMARY KEY,
+        name VARCHAR(100)
+      )
+    ';
+  END
+END!!
+SET TERM ; !!
+''')
+con.commit()
+
+cur.execute("INSERT INTO test_table (id, name) VALUES (1, 'John Doe')")
+cur.execute("INSERT INTO test_table (id, name) VALUES (2, 'Jane Doe')")
+con.commit()
+
+cur.execute("SELECT * FROM test_table")
+for row in cur.fetchall():
+    print(row)
+
+cur.execute("UPDATE test_table SET name = 'John Smith' WHERE id = 1")
+con.commit()
+
+# cur.execute("DELETE FROM test_table WHERE id = 2")
+# con.commit()
+
+cur.close()
+con.close()
+```
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ examples/firebird_demo/main/src/main.cpp
@@ -0,0 +1,183 @@
+/*
+* SPDX-FileCopyrightText: 2024 M5Stack Technology CO LTD
+*
+* SPDX-License-Identifier: MIT
+*/
+
+#include <ibase.h>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+// Firebird API error handling function
+void check_isc_status(ISC_STATUS *status) {
+    if (status[0] == 1 && status[1]) {
+        std::cerr << "Firebird error code: " << status[1] << std::endl;
+        isc_print_status(status);
+        // char err_msg[512];
+        // fb_interpret(err_msg, sizeof(err_msg), &status);
+        // std::cerr << "Firebird error message: " << err_msg << std::endl;
+        exit(1);
+    }
+}
+
+void execute_immediate(isc_db_handle &db, isc_tr_handle &trans,
+                       const char *sql) {
+    ISC_STATUS status[20];
+    if (isc_dsql_execute_immediate(status, &db, &trans, 0, sql, SQL_DIALECT_V6,
+                                   NULL)) {
+        check_isc_status(status);
+    }
+}
+
+void query_table(isc_db_handle &db, isc_tr_handle &trans, const char *sql) {
+    ISC_STATUS status[20];
+    isc_stmt_handle stmt = 0;  // NULL
+    XSQLDA *sqlda        = (XSQLDA *)malloc(XSQLDA_LENGTH(1));
+    sqlda->version       = SQLDA_VERSION1;
+    sqlda->sqln          = 1;
+    sqlda->sqld          = 0;
+
+    if (isc_dsql_allocate_statement(status, &db, &stmt)) {
+        check_isc_status(status);
+    }
+
+    if (isc_dsql_prepare(status, &trans, &stmt, 0, sql, SQL_DIALECT_V6,
+                         sqlda)) {
+        check_isc_status(status);
+    }
+
+    if (sqlda->sqld > sqlda->sqln) {
+        sqlda       = (XSQLDA *)realloc(sqlda, XSQLDA_LENGTH(sqlda->sqld));
+        sqlda->sqln = sqlda->sqld;
+        if (isc_dsql_describe(status, &stmt, SQL_DIALECT_V6, sqlda)) {
+            check_isc_status(status);
+        }
+    }
+
+    for (int i = 0; i < sqlda->sqld; i++) {
+        sqlda->sqlvar[i].sqldata = (char *)malloc(sqlda->sqlvar[i].sqllen);
+        sqlda->sqlvar[i].sqlind  = (short *)malloc(sizeof(short));
+    }
+
+    if (isc_dsql_execute(status, &trans, &stmt, SQL_DIALECT_V6, NULL)) {
+        check_isc_status(status);
+    }
+
+    while (isc_dsql_fetch(status, &stmt, SQL_DIALECT_V6, sqlda) == 0) {
+        for (int i = 0; i < sqlda->sqld; i++) {
+            // XSQLVAR &var = sqlda->sqlvar[i];
+            if (sqlda->sqlvar[i].sqltype == 449) {
+                if (*sqlda->sqlvar[i].sqlind < 0) {
+                    std::cout << "NULL ";
+                } else {
+                    std::cout << std::string(sqlda->sqlvar[i].sqldata,
+                                             sqlda->sqlvar[i].sqllen)
+                              << " ";
+                }
+            } else if (sqlda->sqlvar[i].sqltype == SQL_LONG) {
+                    ISC_LONG data;
+                    memcpy(&data, sqlda->sqlvar[i].sqldata, sizeof(ISC_LONG));
+                    std::cout << data << " ";
+            } else {
+                std::cout << "sqltype: " << sqlda->sqlvar[i].sqltype;
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < sqlda->sqld; i++) {
+        free(sqlda->sqlvar[i].sqldata);
+        free(sqlda->sqlvar[i].sqlind);
+    }
+    free(sqlda);
+
+    if (isc_dsql_free_statement(status, &stmt, DSQL_drop)) {
+        check_isc_status(status);
+    }
+}
+int buildDpb(const char *user, const char *password, char *dpb_buffer) {
+    char *dpb = dpb_buffer;
+
+    *dpb++ = isc_dpb_version1;
+    *dpb++ = isc_dpb_user_name;
+    *dpb++ = strlen(user);
+    strcpy(dpb, user);
+    dpb += strlen(user);
+    *dpb++ = isc_dpb_password;
+    *dpb++ = strlen(password);
+    strcpy(dpb, password);
+    dpb += strlen(password);
+    return dpb - dpb_buffer;
+}
+
+int main() {
+    ISC_STATUS status[20];
+    isc_db_handle db     = 0;
+    isc_tr_handle trans  = 0;
+    const char *db_name  = "127.0.0.1:/opt/firebird/test_database.fdb";
+    const char *user     = "test";
+    const char *password = "test";
+    char dpb_buffer[256];
+    int dpb_buffer_len = buildDpb(user, password, dpb_buffer);
+
+    if (isc_attach_database(status, 0, db_name, &db, dpb_buffer_len,
+                            dpb_buffer)) {
+        check_isc_status(status);
+    }
+
+    if (isc_start_transaction(status, &trans, 1, &db, 0, NULL)) {
+        check_isc_status(status);
+    }
+
+    // Create table
+    execute_immediate(
+        db, trans,
+        "EXECUTE BLOCK AS BEGIN IF (NOT EXISTS (SELECT 1 FROM rdb$relations "
+        "WHERE rdb$relation_name = 'TEST1_TABLE')) THEN BEGIN EXECUTE "
+        "STATEMENT 'CREATE TABLE TEST1_TABLE (id INTEGER NOT NULL, name "
+        "VARCHAR(50), PRIMARY KEY (id))'; END END");
+
+    if (isc_commit_transaction(status, &trans) ||
+        isc_start_transaction(status, &trans, 1, &db, 0, NULL)) {
+        check_isc_status(status);
+    }
+
+    // Insert data
+
+    execute_immediate(db, trans,
+                      "INSERT INTO TEST1_TABLE (id, name) VALUES (1, 'Alice')");
+    execute_immediate(db, trans,
+                      "INSERT INTO TEST1_TABLE (id, name) VALUES (2, 'Bob')");
+    std::cout << "Insert data:" << std::endl;
+    
+    // Query data
+    query_table(db, trans, "SELECT * FROM TEST1_TABLE");
+
+    // Update data
+    execute_immediate(db, trans,
+                      "UPDATE TEST1_TABLE SET name = 'Charlie' WHERE id = 2");
+    // Query data
+    std::cout << "Table content after insert and update:" << std::endl;
+    query_table(db, trans, "SELECT * FROM TEST1_TABLE");
+
+    // Delete data
+    execute_immediate(db, trans, "DELETE FROM TEST1_TABLE WHERE id = 1");
+    // Query data
+    std::cout << "Table content after delete:" << std::endl;
+    query_table(db, trans, "SELECT * FROM TEST1_TABLE");
+
+    execute_immediate(db, trans, "DELETE FROM TEST1_TABLE WHERE id = 2");
+
+    // Commit transaction
+    if (isc_commit_transaction(status, &trans)) {
+        check_isc_status(status);
+    }
+
+    // Disconnect database connection
+    if (isc_detach_database(status, &db)) {
+        check_isc_status(status);
+    }
+
+    return 0;
+}
