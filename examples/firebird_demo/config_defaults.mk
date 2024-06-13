9df925bcbb5b721b2f0d201f26a1bd0983cce207
dianjixz <dianjixz@m5stack.com>
2024-06-13 16:24:49 +0800

    [update] firebird demo compile mode

--- examples/firebird_demo/config_defaults.mk
+++ examples/firebird_demo/config_defaults.mk
@@ -1,4 +1,4 @@
-
+# CONFIG_TOOLCHAIN_PATH="/opt/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin"
 # CONFIG_COMPONENT1_ENABLED=y
 CONFIG_TOOLCHAIN_PREFIX="arm-linux-gnueabihf-"
 CONFIG_TOMMATH_ENABLED=y
