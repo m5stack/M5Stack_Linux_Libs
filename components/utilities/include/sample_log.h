7b475d1919aafd4ccf993e34dbeb5375872bf03a
dianjixz <dianjixz@m5stack.com>
2024-06-18 12:17:32 +0800

    [update] sample_log.h

--- components/utilities/include/sample_log.h
+++ components/utilities/include/sample_log.h
@@ -3,31 +3,36 @@
 
 #include <stdio.h>
 
-typedef enum
-{
-    SAMPLE_LOG_MIN = -1,
+typedef enum {
+    SAMPLE_LOG_MIN       = -1,
     SAMPLE_LOG_EMERGENCY = 0,
-    SAMPLE_LOG_ALERT = 1,
-    SAMPLE_LOG_CRITICAL = 2,
-    SAMPLE_LOG_ERROR = 3,
-    SAMPLE_LOG_WARN = 4,
-    SAMPLE_LOG_NOTICE = 5,
-    SAMPLE_LOG_INFO = 6,
-    SAMPLE_LOG_DEBUG = 7,
+    SAMPLE_LOG_ALERT     = 1,
+    SAMPLE_LOG_CRITICAL  = 2,
+    SAMPLE_LOG_ERROR     = 3,
+    SAMPLE_LOG_WARN      = 4,
+    SAMPLE_LOG_NOTICE    = 5,
+    SAMPLE_LOG_INFO      = 6,
+    SAMPLE_LOG_DEBUG     = 7,
     SAMPLE_LOG_MAX
 } SAMPLE_LOG_LEVEL_E;
 
+#if defined(CONFIG_SAMPLE_LOG_LEVEL)
+static SAMPLE_LOG_LEVEL_E log_level = CONFIG_SAMPLE_LOG_LEVEL;
+#elif defined(CONFIG_SAMPLE_LOG_LEVEL_EXPORT)
+extern SAMPLE_LOG_LEVEL_E log_level;
+#else
 static SAMPLE_LOG_LEVEL_E log_level = SAMPLE_LOG_INFO;
+#endif
 
 #if 1
-#define MACRO_BLACK "\033[1;30;30m"
-#define MACRO_RED "\033[1;30;31m"
-#define MACRO_GREEN "\033[1;30;32m"
+#define MACRO_BLACK  "\033[1;30;30m"
+#define MACRO_RED    "\033[1;30;31m"
+#define MACRO_GREEN  "\033[1;30;32m"
 #define MACRO_YELLOW "\033[1;30;33m"
-#define MACRO_BLUE "\033[1;30;34m"
+#define MACRO_BLUE   "\033[1;30;34m"
 #define MACRO_PURPLE "\033[1;30;35m"
-#define MACRO_WHITE "\033[1;30;37m"
-#define MACRO_END "\033[0m"
+#define MACRO_WHITE  "\033[1;30;37m"
+#define MACRO_END    "\033[0m"
 #else
 #define MACRO_BLACK
 #define MACRO_RED
